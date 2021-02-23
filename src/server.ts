4ad8204ba60a4d1f8ce5475483d7966c1c1c7a07
Rian Marlon <rianmarlon20@yahoo.com>
2021-02-23 15:18:59 -0300

    Adicionando as rotas no server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import 'reflect-metadata';
 import express from 'express';
 import './database';
 
+import router from './routes';
+
 const app = express();
 
-app.listen(3333, () => console.log('Server is running!'));
+app.use(express.json());
+app.use(router);
+
+app.listen(3333);
