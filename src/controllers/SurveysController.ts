8bdff00de3d138a3afbbd87034d34b351982cb5b
Rian Marlon <rianmarlon20@yahoo.com>
2021-02-24 11:47:13 -0300

    Adicionando o SurveysController com o método create

---
+++ src/controllers/SurveysController.ts
@@ -0,0 +1,20 @@
+import { SurveysRepository } from './../repositories/SurveysRepository';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+export class SurveysController {
+  async create (request: Request, response: Response) {
+    const { title, description } = request.body;
+
+    const surveysRepository = getCustomRepository(SurveysRepository);
+
+    const survey = surveysRepository.create({
+      title,
+      description
+    });
+
+    await surveysRepository.save(survey);
+
+    return response.status(201).json(survey);
+  }
+}
