b62fc0ab48010cf47969d1b8035f85cff53f1867
Rian Marlon <rianmarlon20@yahoo.com>
2021-02-28 19:34:20 -0300

    Adicionando o express-async-errors e o errorHandler no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import 'reflect-metadata';
 import express from 'express';
+import 'express-async-errors';
 import createConnection from './database';
 
 import router from './routes';
+import errorHandler from './errors/handler';
 
 createConnection();
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 export { app };
