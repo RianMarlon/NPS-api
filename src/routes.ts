9331890a38ad2407d40b0563dca80ab8dfccb25e
Rian Marlon <rianmarlon20@yahoo.com>
2021-02-28 18:01:51 -0300

    Adicionando a rota answers no routes.tsx

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import { UsersController } from './controllers/UsersController';
 import { SurveysController } from './controllers/SurveysController';
 import { SendMailController } from './controllers/SendMailController';
+import { AnswersController } from './controllers/AnswersController';
 
 const routes = Router();
 
 const usersController = new UsersController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answersController = new AnswersController();
 
 routes.post('/users', usersController.create);
 
@@ -16,5 +18,7 @@ routes.get('/surveys', surveysController.show);
 routes.post('/surveys', surveysController.create);
 
 routes.post('/send-mail', sendMailController.execute);
+
+routes.get('/answers/:value', answersController.execute);
 
 export default routes;
