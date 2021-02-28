b58357e6cb39d3af80d264b33dce3ae30676df4b
Rian Marlon <rianmarlon20@yahoo.com>
2021-02-28 13:40:39 -0300

    Adicionando a rota send-mail

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,19 @@ import { Router } from 'express';
 
 import { UsersController } from './controllers/UsersController';
 import { SurveysController } from './controllers/SurveysController';
+import { SendMailController } from './controllers/SendMailController';
 
 const routes = Router();
 
 const usersController = new UsersController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 routes.post('/users', usersController.create);
 
 routes.get('/surveys', surveysController.show);
 routes.post('/surveys', surveysController.create);
+
+routes.post('/send-mail', sendMailController.execute);
 
 export default routes;
