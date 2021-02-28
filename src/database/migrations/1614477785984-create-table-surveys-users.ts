4d827a681110d5689d171a61ab184f1626b77922
Rian Marlon <rianmarlon20@yahoo.com>
2021-02-27 23:45:16 -0300

    Adicionando a migration de criação da tabela surveys_users

---
+++ src/database/migrations/1614477785984-create-table-surveys-users.ts
@@ -0,0 +1,56 @@
+import {MigrationInterface, QueryRunner, Table} from 'typeorm';
+
+export class createTableSurveysUsers1614477785984 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'surveys_users',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true
+        },
+        {
+          name: 'value',
+          type: 'number',
+          isNullable: true
+        },
+        {
+          name: 'user_id',
+          type: 'uuid'
+        },
+        {
+          name: 'survey_id',
+          type: 'uuid'
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'current_timestamp'
+        }
+      ],
+      foreignKeys: [
+        {
+          name: 'fk_surveys_users_users',
+          referencedTableName: 'users',
+          referencedColumnNames: ['id'],
+          columnNames: ['user_id'],
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+        },
+        {
+          name: 'fk_surveys_users_surveys',
+          referencedTableName: 'surveys',
+          referencedColumnNames: ['id'],
+          columnNames: ['survey_id'],
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+        }
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('surveys_users');
+  }
+}
